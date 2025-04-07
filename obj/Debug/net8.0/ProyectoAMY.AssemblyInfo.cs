b1c9bb9d180f2fd8337df1c3a144ff91050addce
BaltaRisso <rissopatronivan62@gmail.com>
2025-04-07 16:53:53 -0300

    cambio url de img

--- obj/Debug/net8.0/ProyectoAMY.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoAMY.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAMY")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8670e9815c681312000e56ff1fdc204553ed7f2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+696e8e64dd0ff69c025f3b8fe92ecd7d8bc93199")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAMY")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAMY")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
