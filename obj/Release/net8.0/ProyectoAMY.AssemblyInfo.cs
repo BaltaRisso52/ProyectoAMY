3def27e0f860c8c217060e97410523490afff148
BaltaRisso <rissopatronivan62@gmail.com>
2025-04-08 23:21:13 -0300

    prueba de actualizacion

--- obj/Release/net8.0/ProyectoAMY.AssemblyInfo.cs
+++ obj/Release/net8.0/ProyectoAMY.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAMY")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5cfd71bd55d1cb2c47609c5d0d12236aecdf15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b70d28cb30a665fc2fb3048faa0756f7d01e040")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAMY")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAMY")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
